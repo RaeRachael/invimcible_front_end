4144cf77890607eeb966598daaa4c7beae7a908a
Rachael <rachaelewins@hotmail.com>
2020-08-11 15:41:45 +0100

    Tests for basic movement, from attempMove

--- src/features/player/playerMovemnet.test.js
+++ src/features/player/playerMovemnet.test.js
@@ -5,25 +5,70 @@ import store from "../../config/store";
 
 describe("Player", () => {
 
-  it("can move east", () => {
+  describe("basic movement", function() {
+
     store.dispatch({
       type: "UPDATE_MAP_STORE",
       payload: {
         loaded: true,
-        tiles: [[{"value":"SC4","x":32,"y":224,"blocked":false},{"value":"SPD","x":96,"y":224,"blocked":false}]],
+        tiles: [[{"value":"SC4","x":32,"y":224,"blocked":false},{"value":"SPD","x":96,"y":224,"blocked":false}],
+                [{"value":"SC4","x":32,"y":224,"blocked":false},{"value":"SPD","x":96,"y":224,"blocked":false}]],
         level: 1
       },
     })
-    store.dispatch({
-      type: "UPDATE_PLAYER_STORE",
-      payload: {
-        position: [0, 0],
-            walkIndex: 0
-      }
-    })
-    attemptMove("EAST")
 
-    expect(store.getState().player.position).toEqual([32, 0])
+    it("can move east", () => {
+      store.dispatch({
+        type: "UPDATE_PLAYER_STORE",
+        payload: {
+          position: [0, 0],
+          walkIndex: 0
+        }
+      })
+      attemptMove("EAST")
+
+      expect(store.getState().player.position).toEqual([32, 0])
+    });
+
+    it("can move west", () => {
+      store.dispatch({
+        type: "UPDATE_PLAYER_STORE",
+        payload: {
+          position: [32, 0],
+          walkIndex: 0
+        }
+      })
+      attemptMove("WEST")
+
+      expect(store.getState().player.position).toEqual([0, 0])
+    });
+
+    it("can move south", () => {
+      store.dispatch({
+        type: "UPDATE_PLAYER_STORE",
+        payload: {
+          position: [0, 0],
+          walkIndex: 0
+        }
+      })
+      attemptMove("SOUTH")
+
+      expect(store.getState().player.position).toEqual([0, 32])
+    });
+
+    it("can move north", () => {
+      store.dispatch({
+        type: "UPDATE_PLAYER_STORE",
+        payload: {
+          position: [0, 32],
+          walkIndex: 0
+        }
+      })
+      attemptMove("NORTH")
+
+      expect(store.getState().player.position).toEqual([0, 0])
+    });
+  
   });
   
 });
