2c01ac155a1c97e9236b90cce53ed1313da26627
Rachael <rachaelewins@hotmail.com>
2020-08-13 11:49:29 +0100

    Formatting of test file

--- src/features/player/playerMovemnet.test.js
+++ src/features/player/playerMovemnet.test.js
@@ -1,98 +1,96 @@
 import React from "react";
-import { attemptMove, attemptJump, handleKeyDown } from "./movement.js";
+import { handleKeyDown } from "./movement.js";
 import { addToStoreBlockingTiles, addToStoreLocation } from "./testHelper.js"
 import store from "../../config/store";
 
 
-describe("Player", () => {
+describe("basic movement", function() {
 
-  describe("basic movement", function() {
+  addToStoreBlockingTiles([[false, false],[false, false]])
 
-    addToStoreBlockingTiles([[false, false],[false, false]])
+  it("can move east", () => {
+    addToStoreLocation([0, 0])
+    handleKeyDown({key: "l"})
 
-    it("can move east", () => {
-      addToStoreLocation([0, 0])
-      handleKeyDown({key: "l"})
-
-      expect(store.getState().player.position).toEqual([32, 0])
-    });
-
-    it("can move west", () => {
-      addToStoreLocation([32, 0])
-      handleKeyDown({key: "h"})
-
-      expect(store.getState().player.position).toEqual([0, 0])
-    });
-
-    it("can move south", () => {
-      addToStoreLocation([0, 0])
-      handleKeyDown({key: "j"})
-
-      expect(store.getState().player.position).toEqual([0, 32])
-    });
-
-    it("can move north", () => {
-      addToStoreLocation([0, 32])
-      handleKeyDown({key: "k"})
-
-      expect(store.getState().player.position).toEqual([0, 0])
-    });
-  
+    expect(store.getState().player.position).toEqual([32, 0])
   });
 
-  describe("cases where player can't move", function() {
+  it("can move west", () => {
+    addToStoreLocation([32, 0])
+    handleKeyDown({key: "h"})
 
-    it("can not move off the map", function() {
-      addToStoreBlockingTiles([[false]])
+    expect(store.getState().player.position).toEqual([0, 0])
+  });
+
+  it("can move south", () => {
+    addToStoreLocation([0, 0])
+    handleKeyDown({key: "j"})
+
+    expect(store.getState().player.position).toEqual([0, 32])
+  });
+
+  it("can move north", () => {
+    addToStoreLocation([0, 32])
+    handleKeyDown({key: "k"})
+
+    expect(store.getState().player.position).toEqual([0, 0])
+  });
+
+});
+
+describe("cases where player can't move", function() {
+
+  it("can not move off the map", function() {
+    addToStoreBlockingTiles([[false]])
+    addToStoreLocation([0, 0])
+    handleKeyDown({key: "k"})
+
+
+    expect(store.getState().player.position).toEqual([0, 0])
+  })
+
+  it("can not move onto a blocking tile", function() {
+    addToStoreBlockingTiles([[false, true]])
+    addToStoreLocation([0, 0])
+    handleKeyDown({key: "l"})
+
+
+    expect(store.getState().player.position).toEqual([0, 0])
+  })
+
+})
+
+describe("'w' and 'b' inputs", function() {
+
+  describe("cannot jump if not next to a bin", function() {
+
+    it("does not jump with 'w' command unless next to a bin", function() {
+      addToStoreBlockingTiles([[false, false, false]])
       addToStoreLocation([0, 0])
-      handleKeyDown({key: "k"})
-
+      handleKeyDown({key: "w"})
 
       expect(store.getState().player.position).toEqual([0, 0])
     })
 
-    it("can not move onto a blocking tile", function() {
-      addToStoreBlockingTiles([[false, true]])
-      addToStoreLocation([0, 0])
-      handleKeyDown({key: "l"})
+    it("does not jump with 'b' command unless next to a bin", function() {
+      addToStoreBlockingTiles([[false, false, false]])
+      addToStoreLocation([64, 0])
+      handleKeyDown({key: "b"})
 
-
-      expect(store.getState().player.position).toEqual([0, 0])
+      expect(store.getState().player.position).toEqual([64, 0])
     })
 
   })
 
-  describe("'w' and 'b' inputs", function() {
+  describe("can jump over bins with the commands 'w' and 'b'", function() {
 
-    describe("cannot jump if not next to a bin", function() {
+    it("jumps with 'w' command when next to a bin", function() {
+      addToStoreBlockingTiles([[false, true, false]])
+      addToStoreLocation([0, 0])
+      handleKeyDown({key: "w"})
 
-      it("does not jump with 'w' command unless next to a bin", function() {
-        addToStoreBlockingTiles([[false, false, false]])
-        addToStoreLocation([0, 0])
-        handleKeyDown({key: "w"})
-
-        expect(store.getState().player.position).toEqual([0, 0])
-      })
-
-      it("does not jump with 'b' command unless next to a bin", function() {
-        addToStoreBlockingTiles([[false, false, false]])
-        addToStoreLocation([64, 0])
-        handleKeyDown({key: "b"})
-
-        expect(store.getState().player.position).toEqual([64, 0])
-      })
-
+      expect(store.getState().player.position).toEqual([64, 0])
     })
-
-    describe("can jump over bins with the commands 'w' and 'b'", function() {
-
-      it("jumps with 'w' command when next to a bin", function() {
-        addToStoreBlockingTiles([[false, true, false]])
-        addToStoreLocation([0, 0])
-        handleKeyDown({key: "w"})
-
-        expect(store.getState().player.position).toEqual([64, 0])
-      })
 
 
     it("jumps with 'b' command when next to a bin", function() {
@@ -102,24 +100,8 @@ describe("Player", () => {
 
       expect(store.getState().player.position).toEqual([0, 0])
     })
-  })
-
-
-  // })
-
 
   })
-
-  // describe("can jump over bins with the commands 'w' and 'b'", function() {
-
-  //   it("does not jump with 'w' command unless next to a bin", function() {
-  //     addToStoreBlockingTiles([[false, false, false]])
-  //     addToStoreLocation([0, 0])
-  //     handleKeyDown({key: "w"})
-
-  //     expect(store.getState().player.position).toEqual([0, 0])
-  //   })
   
-  // })
+})
 
-});
