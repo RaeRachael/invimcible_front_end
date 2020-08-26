6ed3d04c079d3e8130d881471874e53138c7f52f
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-08-26 11:59:55 +0100

    test -renders team members names

--- src/components/about/about.test.js
+++ src/components/about/about.test.js
@@ -4,12 +4,19 @@ import { shallow } from 'enzyme'
 
 describe('About', () => {
 
-  it("renders without crashing", () => {
-    shallow(<About />);
-  });
   it("renders team information", () => {
     const wrapper = shallow(<About />);
     const info = <p> Hi, we are the creators of The adventures of Vim. </p>
     expect(wrapper.contains(info)).toEqual(true);
   })
+
+  it("renders team names", () => {
+    const wrapper = shallow(<About />);
+    const names = ["<p1>Sophie Brown</p1>", "<p1>Al Sumner</p1>", "<p1>Tristan Langford</p1>", "<p1>Rachael Ewins</p1>", "<p1>Nadine Loepfe</p1>", "<p1>Katie McDonagh</p1>"]
+    for(let i = 0; i ++; i < names.length){
+      expect(wrapper.contains(names[i])).toEqual(true);
+    }
+  })
+
+
 })
