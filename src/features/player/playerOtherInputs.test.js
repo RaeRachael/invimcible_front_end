b0dec6cce2ddefb22d3b7f6852b466eacc281f3f
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-08-26 11:23:30 +0100

    change name of tests, and add handle key down to test to stop Vim moving

--- src/features/player/playerOtherInputs.test.js
+++ src/features/player/playerOtherInputs.test.js
@@ -8,17 +8,25 @@ describe('Other key press inputs', function() {
     expect(handleKeyDown({ key: "z" })).toEqual("not in use")
   })
 
-  it('Should test backspace', function() {
+  it('If text area contains more than one character, backspace should not allow Vim to move', function() {
+    addToStoreTiles([[false, false],[false, false]])
+    handleKeyDown({key: ":"})
     store.dispatch({
       type: "ADD_LETTER",
       letters: ":h",
     })
-
     expect(handleKeyDown({ key: "Backspace"})).toEqual("text area still in use")
+
+    addToStoreLocation([0, 0])
+    handleKeyDown({key: "j"})
+
+    expect(store.getState().player.position).toEqual([0, 0])
+
   })
 
-  it('Should test backspace', function() {
+  it('If text area contains one character, backspace should allow Vim to move', function() {
     addToStoreTiles([[false, false],[false, false]])
+    handleKeyDown({key: ":"})
     store.dispatch({
       type: "ADD_LETTER",
       letters: ":",
